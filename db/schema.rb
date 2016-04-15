861ad2ceb13057bb41f3cdffa41fd4f6bfd7aaa6
Yazed <Jamal>
2016-04-15 11:31:22 +0800

    able to record some transaction data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413065554) do
+ActiveRecord::Schema.define(version: 20160415013333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,20 +41,16 @@ ActiveRecord::Schema.define(version: 20160413065554) do
     t.json     "not_avail"
   end
 
-  create_table "pictures", force: :cascade do |t|
-    t.integer  "list_id"
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "list_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "start_book"
     t.string   "end_book"
+    t.string   "status",         default: "open"
+    t.string   "total"
+    t.string   "transaction_id"
   end
 
   create_table "users", force: :cascade do |t|
