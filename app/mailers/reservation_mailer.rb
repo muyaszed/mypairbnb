996890b8ed1dcc7988e01cba28a0440f75f23cb4
Yazed <Jamal>
2016-04-19 15:21:19 +0800

    added email feature when making reservation

---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,27 @@
+class ReservationMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.reservation.Mailer.subject
+  #
+  def Mailer
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+
+  def booking_email(customer, host, reservation_id, total_amount, listing_link, reservation_link)
+
+  	@customer = customer
+  	@host = host
+  	@id = reservation_id
+  	@total = total_amount
+  	@reservation_link = reservation_link
+  	@listing_link = listing_link
+
+  	mail(to: @host.email, subject:"You've got a reservation for PairVnV")
+
+  end
+end
